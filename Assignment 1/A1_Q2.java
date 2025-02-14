07d9070e9b095bee427307074b20251379764487
subha03 <roysubhajit2003@gmail.com>
2025-02-14 21:40:32 +0530

    Done

---
+++ Assignment 1/A1_Q2.java
@@ -0,0 +1,10 @@
+
+public class A1_Q2 {
+  public static void main(String[] args) {
+    if(args.length>0){
+        System.err.println(args[0]);
+    }else{
+        System.out.println("No argument provided");
+    }
+  }
+}
