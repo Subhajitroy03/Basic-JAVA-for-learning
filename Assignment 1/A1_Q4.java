69790d8f5aa259c11f1e59bd592f15108b38db17
subha03 <roysubhajit2003@gmail.com>
2025-02-14 22:06:42 +0530

    Done

---
+++ Assignment 1/A1_Q4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class A1_Q4 {
+  public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter your price of item: ");
+    double price=sc.nextDouble();
+    System.out.print("price of item in paise: "+(int)price*100);
+  }
+}
---
+++ Assignment 1/tempCodeRunnerFile.java
@@ -0,0 +1,2 @@
+ static void main(String[] args) {
+    System.out.println("Subhajit Roy");
