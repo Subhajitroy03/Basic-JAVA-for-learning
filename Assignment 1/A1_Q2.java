6f1aee7b7f309c5f4335453737826f9afc24c0a2
subha03 <roysubhajit2003@gmail.com>
2025-02-14 21:41:49 +0530

    Done

--- Assignment 1/A1_Q2.java
+++ Assignment 1/A1_Q2.java
@@ -1,8 +1,14 @@
 
+
 public class A1_Q2 {
   public static void main(String[] args) {
     if(args.length>0){
         System.err.println(args[0]);
+        /*
+TO RUN
+1) javac A1_Q2.java
+2) java A1_Q2 "Academy of Technology"
+*/
     }else{
         System.out.println("No argument provided");
     }
