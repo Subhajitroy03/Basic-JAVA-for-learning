28c579480e5e8be803d88d6eb389109fc4886e1f
subha03 <roysubhajit2003@gmail.com>
2025-02-14 21:48:13 +0530

    Done

---
+++ Assignment 1/A1_Q3.java
@@ -0,0 +1,16 @@
+
+public class A1_Q3 {
+  public static void main(String[] args) {
+    if(args.length==2){
+        System.err.println(args[0]+" is a BTECH student of "+args[1]+" college");
+
+    }else{
+        System.out.println("Invalid");
+    }
+  }
+}
+/*
+TO RUN: 
+1)javac A1_Q3.java
+2)java A1_Q3 "Subhajit"  "Academy of Technology"
+*/
