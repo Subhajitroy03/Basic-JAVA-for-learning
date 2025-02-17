e04a7c5a62c8f82fceac73ad6076e4c8c5922dd0
subha03 <roysubhajit2003@gmail.com>
2025-02-17 12:55:19 +0530

    done2

---
+++ Assignment 1/A1_Q7.java
@@ -0,0 +1,41 @@
+public class A1_Q7 {
+  public static void main(String[] args) {
+    int a,b,result;
+    if(args.length>=3){
+        a=Integer.parseInt(args[0]);
+        b=Integer.parseInt(args[2]);
+        String operator=args[1];
+        switch(operator){
+            case "+":
+                result=a+b;
+                break;
+            case "-":
+                result=a-b;
+                break;
+            case "X":
+                result=a*b;
+                break;
+            case "/":
+                result=a/b;
+                break;
+            default:
+                result=0;
+                System.err.println("Invalid operator");
+
+        }
+        System.err.println("The result is "+result);
+
+
+    }else{
+        System.err.println("Invalid expression"); 
+    }
+  }
+}
+/*
+HOW TO RUN
+javac A1_Q7.java 
+java A1_Q7 7 + 10
+java A1_Q7 7 - 10
+java A1_Q7 7 X 10  (PowerShell expands * to match filenames in the directory)
+java A1_Q7 7 / 10
+*/
